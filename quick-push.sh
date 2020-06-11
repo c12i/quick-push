58493f1e7c366b8ac6ddd88ad37c344db34640b0
Collins Muriuki <murerwacollins@gmail.com>
2020-06-11 18:55:33 +0300

    2020-06-11_18-28

--- quick-push.sh
+++ quick-push.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-git_current_branch=`echo $(git symbolic-ref -q HEAD) | cut -d "/" -f 3`
+working_git_branch=`echo $(git symbolic-ref -q HEAD) | cut -d "/" -f 3`
 
 if [ $# -eq 0 ]
     then
-        git add . && git commit -m $(date +"%Y-%m-%d_%H-%M") && git push origin $git_current_branch
+        git add . && git commit -m $(date +"%Y-%m-%d_%H-%M") && git push origin $working_git_branch
     else
-        git add . && git commit -m "$@" && git push origin $git_current_branch
+        git add . && git commit -m "$@" && git push origin $working_git_branch
 fi
