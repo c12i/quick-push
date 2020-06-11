93d989e5b3e73c6ce7b3e38f3bc2451a01fed4f4
Collins Muriuki <murerwacollins@gmail.com>
2020-06-11 18:42:07 +0300

    testing

--- quick-push.sh
+++ quick-push.sh
@@ -4,7 +4,7 @@ git_current_branch=`echo $(git symbolic-ref -q HEAD) | cut -d "/" -f 3`
 
 if [ -z $* ]
     then
-        git add . && git commit -m $* && git push origin $git_current_branch
-    else
         git add . && git commit -m $(date +"%Y-%m-%d_%H-%M") && git push origin $git_current_branch
+    else
+        git add . && git commit -m $* && git push origin $git_current_branch
 fi
