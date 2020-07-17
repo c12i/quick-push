f1d0c4a05309cf14718b9dc31919f99b52ac9aee
Collins Muriuki <murerwacollins@gmail.com>
2020-07-17 19:24:07 +0300

    Fix issue #1

--- quick-push.sh
+++ quick-push.sh
@@ -4,35 +4,32 @@ remote=`git remote`
 working_git_branch=`echo $(git symbolic-ref -q HEAD) | cut -d "/" -f 3`
 fatal="fatal: not a git repository (or any of the parent directories): .git"
 
-function only_commit() {
+case $remote in
+  fatal)
     if [ $# -eq 0 ]
     then
         git add . && git commit -m $(date +"%Y-%m-%d_%H-%M")
     else
         git add . && git commit -m "$@"
     fi
-}
+    echo "No remote repo, only committing"
+    ;;
 
-function commit_and_push() {
+  origin)
     if [ $# -eq 0 ]
     then
         git add . && git commit -m $(date +"%Y-%m-%d_%H-%M") && git push origin $working_git_branch
     else
         git add . && git commit -m "$@" && git push origin $working_git_branch
     fi
-}
-
-case $remote in
-  fatal)
-    only_commit
-    echo "No remote repo, only committing"
-    ;;
-
-  origin)
-    commit_and_push
     ;;
 
   *)
-    commit_and_push
+    if [ $# -eq 0 ]
+    then
+        git add . && git commit -m $(date +"%Y-%m-%d_%H-%M") && git push origin $working_git_branch
+    else
+        git add . && git commit -m "$@" && git push origin $working_git_branch
+    fi
     ;;
 esac
