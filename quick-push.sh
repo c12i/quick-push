a02a2aa0bb6e749772f8fde9e5ba4e4f0ed35b9c
Collins Muriuki <murerwacollins@gmail.com>
2020-07-17 19:12:28 +0300

    2020-07-17_19-12

--- quick-push.sh
+++ quick-push.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
+remote=`git remote`
 working_git_branch=`echo $(git symbolic-ref -q HEAD) | cut -d "/" -f 3`
 
-if [ $# -eq 0 ]
+function only_commit() {
+    if [ $# -eq 0 ]
+    then
+        git add . && git commit -m $(date +"%Y-%m-%d_%H-%M")
+    else
+        git add . && git commit -m "$@"
+    fi
+}
+
+function commit_and_push() {
+    if [ $# -eq 0 ]
     then
         git add . && git commit -m $(date +"%Y-%m-%d_%H-%M") && git push origin $working_git_branch
     else
         git add . && git commit -m "$@" && git push origin $working_git_branch
-fi
+    fi
+}
+
+case $remote in
+
+  "fatal: not a git repository (or any of the parent directories): .git")
+    only_commit
+    ;;
+
+  *)
+    commit_and_push
+    ;;
+esac
